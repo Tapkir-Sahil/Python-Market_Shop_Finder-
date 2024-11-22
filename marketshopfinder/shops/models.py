97e2e261e9ac91995eb97a22e488016423506914
sahil-tapkir <sahiltapkir07@gmail.com>
2024-11-22 13:29:16 +0530

    created database and jinga templates

--- marketshopfinder/shops/models.py
+++ marketshopfinder/shops/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ShopVariety(models.Model):
+    SHOP_TYPE_LOC=[
+        ('PU','Pune'),
+        ('MU','Mumbai'),
+        ('NA','Nagpur'),
+        ('KO','Kokan'),
+        ('SA','Satara'),
+    ]
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='typeshop/')
+    date_added=models.DateTimeField(default=timezone.now)
+    type=models.CharField(max_length=9,choices=SHOP_TYPE_LOC,default='PU')
+    description=models.TextField()
+    def __str__(self):
+     return self.name
+    class Meta:
+        verbose_name = "Shop Variety"
+        verbose_name_plural = "Shop Varieties"
