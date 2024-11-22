78a0af38cfe827fb2016714defe7ff34583e48c0
sahil-tapkir <sahiltapkir07@gmail.com>
2024-11-22 16:08:10 +0530

    Added Database with admin registrations

--- marketshopfinder/marketshopfinder/settings.py
+++ marketshopfinder/marketshopfinder/settings.py
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL='/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
+MEDIA_URLS='/media'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- marketshopfinder/marketshopfinder/urls.py
+++ marketshopfinder/marketshopfinder/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,5 @@ urlpatterns = [
     path('',include('shops.urls')),
     # path('about/',views.About,name='about'),
     # path('contact/',views.Contact,name='contact')
-]
+
+]+static(settings.MEDIA_URLS,document_root=settings.MEDIA_ROOT)
