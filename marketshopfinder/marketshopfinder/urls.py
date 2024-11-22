a1c7d8c3948cd0dc99108449f7b8ffafa6d67ae4
sahil-tapkir <sahiltapkir07@gmail.com>
2024-11-22 18:01:36 +0530

    Added Images

--- marketshopfinder/marketshopfinder/urls.py
+++ marketshopfinder/marketshopfinder/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     # path('about/',views.About,name='about'),
     # path('contact/',views.Contact,name='contact')
 
-]+static(settings.MEDIA_URLS,document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- marketshopfinder/shops/views.py
+++ marketshopfinder/shops/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import ShopVariety
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    typeshop = ShopVariety.objects.all()  # Fetch all shop data
+    return render(request, 'home.html', {'typeshop': typeshop})
+
+# def all_shops(request):
+#     typeshop = ShopVariety.objects.all()
+#     return render(request, 'shops/templates/home.html',{'typeshop':typeshop})
