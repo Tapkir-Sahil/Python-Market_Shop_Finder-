dde317b92d1933f13c3ab147fc6d8bb3311995b6
sahil-tapkir <sahiltapkir07@gmail.com>
2024-11-22 22:13:56 +0530

    nothing much

--- marketshopfinder/shops/views.py
+++ marketshopfinder/shops/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import ShopVariety
+from django.shortcuts import HttpResponse
+from django.template.loader import get_template
 
 # Create your views here.
 def home(request):
     typeshop = ShopVariety.objects.all()  # Fetch all shop data
     return render(request, 'home.html', {'typeshop': typeshop})
-
-# def all_shops(request):
-#     typeshop = ShopVariety.objects.all()
-#     return render(request, 'shops/templates/home.html',{'typeshop':typeshop})
