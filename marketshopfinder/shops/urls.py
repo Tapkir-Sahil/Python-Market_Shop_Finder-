a7b62798b4f73a1eb6401b072fea5aa8fea8db71
sahil-tapkir <sahiltapkir07@gmail.com>
2024-11-23 12:17:45 +0530

    Fix the detail page and it is running now

--- marketshopfinder/shops/urls.py
+++ marketshopfinder/shops/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('<int:detail_id>/',views.detail,name='detail'),
 ]
--- marketshopfinder/shops/views.py
+++ marketshopfinder/shops/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,get_object_or_404
 from .models import ShopVariety
 from django.shortcuts import HttpResponse
-from django.template.loader import get_template
 
 # Create your views here.
 def home(request):
     typeshop = ShopVariety.objects.all()  # Fetch all shop data
-    return render(request, 'home.html', {'typeshop': typeshop})
+    return render(request, 'shops\home.html', {'typeshop': typeshop})
+
+def detail(request,detail_id):
+    shop = get_object_or_404(ShopVariety,pk=detail_id)
+    return render(request, 'shops/detail.html', {'shop':shop})
