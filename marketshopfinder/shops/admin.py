e3666987f28c9112eb3551c1e3493ce44d697020
sahil-tapkir <sahiltapkir07@gmail.com>
2024-11-23 19:27:32 +0530

    Updated Database added stores certificate and shopreview

--- marketshopfinder/shops/admin.py
+++ marketshopfinder/shops/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
-from .models import ShopVariety
+from .models import ShopVariety,Shop_review,Store,Cerificate
 # Register your models here.
-admin.site.register(ShopVariety)
+
+
+class Shop_reviewInline(admin.TabularInline):
+    model=Shop_review
+    extra=1
+
+class ShopVarityAdmin(admin.ModelAdmin):
+    list_display=('name','type','date_added')
+    inlines=[Shop_reviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display=('name','location')
+    filter_horizontal=('shopvarities',)
+
+class CerificateAdmin(admin.ModelAdmin):
+    list_display=('shop','Cnumber','issued_date','valid_until')
+    
+    
+    
+admin.site.register(ShopVariety,ShopVarityAdmin)
+admin.site.register(Store,StoreAdmin)
+admin.site.register(Cerificate,CerificateAdmin)
--- marketshopfinder/shops/models.py
+++ marketshopfinder/shops/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
 class ShopVariety(models.Model):
     SHOP_TYPE_LOC=[
@@ -20,3 +20,34 @@ class ShopVariety(models.Model):
     class Meta:
         verbose_name = "Shop Variety"
         verbose_name_plural = "Shop Varieties"
+
+#one to many
+class Shop_review(models.Model):
+   shop=models.ForeignKey(ShopVariety,on_delete=models.CASCADE,related_name='reviews')
+   user=models.ForeignKey(User,on_delete=models.CASCADE)
+   rating=models.IntegerField()
+   date_added=models.DateField(default=timezone.now)
+
+   def __str__(self):
+      return f'{self.user.username} review for {self.shop.name}'
+
+
+#many to many
+class Store(models.Model):
+   name=models.CharField(max_length=100)
+   location=models.CharField(max_length=100)
+   shopvarities=models.ManyToManyField(ShopVariety,related_name='stores')
+
+   def __str__(self):
+      return self.name
+
+#one to one
+
+class Cerificate(models.Model):
+   shop=models.OneToOneField(ShopVariety,on_delete=models.CASCADE,related_name='certificate')
+   Cnumber=models.CharField(max_length=50)
+   issued_date=models.DateTimeField(default=timezone.now)
+   valid_until=models.DateField()
+
+   def __str__(self):
+      return f'Certifate for {self.shop.name}'
