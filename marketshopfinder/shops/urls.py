feb116428d49c9e8ee9b7d97f40a11cce98b0eaf
sahil-tapkir <sahiltapkir07@gmail.com>
2024-11-24 16:03:04 +0530

    Added a styling and some buttons like serach and redesign the detail page

--- marketshopfinder/shops/urls.py
+++ marketshopfinder/shops/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('<int:detail_id>/',views.detail,name='detail'),
+    path('shopstores/',views.shop_store_view,name='shopstores')
 ]
--- marketshopfinder/shops/views.py
+++ marketshopfinder/shops/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from .models import ShopVariety
+from .models import ShopVariety,Store
 from django.shortcuts import HttpResponse
+from .forms import ShopVarietyForm
 
 # Create your views here.
 def home(request):
@@ -10,3 +11,14 @@ def home(request):
 def detail(request,detail_id):
     shop = get_object_or_404(ShopVariety,pk=detail_id)
     return render(request, 'shops/detail.html', {'shop':shop})
+
+def shop_store_view(request):
+    stores=None
+    if request.method == 'POST':
+        form = ShopVarietyForm(request.POST)
+        if form.is_valid():
+            shop_variety=form.cleaned_data['shop_variety']
+            stores=Store.objects.filter(shopvarities=shop_variety)
+    else:
+        form=ShopVarietyForm()
+    return render(request,'shops/shopstores.html',{'form':form,'stores':stores})
